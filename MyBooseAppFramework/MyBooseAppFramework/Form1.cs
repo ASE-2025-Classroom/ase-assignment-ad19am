268d8407f20244597052374bef35a42eb9115c1b
ad19am <147050012+ad19am@users.noreply.github.com>
2025-10-11 16:05:17 +0100

    Implement Paint event and basic GDI drawing

--- MyBooseAppFramework/MyBooseAppFramework/Form1.cs
+++ MyBooseAppFramework/MyBooseAppFramework/Form1.cs
@@ -15,6 +15,25 @@ namespace MyBooseAppFramework
         public Form1()
         {
             InitializeComponent();
+            this.Paint += Form1_Paint;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+ 
+            e.Graphics.FillRectangle(Brushes.White, this.ClientRectangle);
+ 
+            using (var red = new Pen(Color.Red, 2))
+                e.Graphics.DrawLine(red, 10, 10, 200, 10);
+
+            e.Graphics.DrawString("Hello, Windows Forms!",
+                new Font("Arial", 12), Brushes.Black, 10, 30);
+
+            using (var blue = new Pen(Color.Blue, 3))
+                e.Graphics.DrawRectangle(blue, 50, 60, 150, 90);
+
+            using (var green = new Pen(Color.Green, 3))
+                e.Graphics.DrawEllipse(green, 230, 60, 100, 100);
         }
     }
 }
