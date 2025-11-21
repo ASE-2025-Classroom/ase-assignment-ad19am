e8efd11cf57da1cb090e19080a6ce2c1b1c94c04
ad19am <a.shaban9452@student.leedsbeckett.ac.uk>
2025-11-21 15:13:31 +0000

    Screenshots for restricted /unrestricted drawing demonstration

--- MyBooseAppUI/Form1.Designer.cs
+++ MyBooseAppUI/Form1.Designer.cs
@@ -38,39 +38,44 @@
             // txtProgram
             // 
             this.txtProgram.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtProgram.Location = new System.Drawing.Point(27, 12);
+            this.txtProgram.Location = new System.Drawing.Point(40, 18);
+            this.txtProgram.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtProgram.Multiline = true;
             this.txtProgram.Name = "txtProgram";
             this.txtProgram.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtProgram.Size = new System.Drawing.Size(400, 180);
+            this.txtProgram.Size = new System.Drawing.Size(598, 275);
             this.txtProgram.TabIndex = 0;
+            this.txtProgram.Text = "5";
             this.txtProgram.TextChanged += new System.EventHandler(this.txtProgram_TextChanged);
             // 
             // txtOutput
             // 
-            this.txtOutput.Location = new System.Drawing.Point(27, 198);
+            this.txtOutput.Location = new System.Drawing.Point(40, 305);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
             this.txtOutput.ReadOnly = true;
             this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtOutput.Size = new System.Drawing.Size(400, 120);
+            this.txtOutput.Size = new System.Drawing.Size(598, 182);
             this.txtOutput.TabIndex = 1;
             // 
             // canvas
             // 
             this.canvas.BackColor = System.Drawing.Color.White;
             this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.canvas.Location = new System.Drawing.Point(27, 370);
+            this.canvas.Location = new System.Drawing.Point(40, 569);
+            this.canvas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.canvas.Name = "canvas";
-            this.canvas.Size = new System.Drawing.Size(400, 400);
+            this.canvas.Size = new System.Drawing.Size(599, 614);
             this.canvas.TabIndex = 2;
             this.canvas.TabStop = false;
             // 
             // btnRun
             // 
-            this.btnRun.Location = new System.Drawing.Point(27, 324);
+            this.btnRun.Location = new System.Drawing.Point(40, 498);
+            this.btnRun.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnRun.Name = "btnRun";
-            this.btnRun.Size = new System.Drawing.Size(100, 40);
+            this.btnRun.Size = new System.Drawing.Size(150, 62);
             this.btnRun.TabIndex = 3;
             this.btnRun.Text = "Run";
             this.btnRun.UseVisualStyleBackColor = true;
@@ -78,13 +83,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(559, 782);
+            this.ClientSize = new System.Drawing.Size(838, 1050);
             this.Controls.Add(this.btnRun);
             this.Controls.Add(this.canvas);
             this.Controls.Add(this.txtOutput);
             this.Controls.Add(this.txtProgram);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
